eb87b0d451e7d391b7e126c746c2ca2f40be87d4
Nahil Sobh <nahil.sobh@gmail.com>
2017-08-31 13:40:40 -0500

    added output for cluster label to each sampel id

--- test/integration/verify_benchmarks.py
+++ test/integration/verify_benchmarks.py
@@ -30,22 +30,25 @@ def main():
                                  'BENCHMARK_1_kmeans.yml'
                                , 'rows_averages_by_cluster_kmeans'
                                , 'rows_by_columns_heatmap_kmeans'
-                               , 'top_rows_by_cluster_kmeans'
                                , 'rows_variance_kmeans'
+                               , 'samples_label_by_cluster_kmeans'
+                               , 'top_rows_by_cluster_kmeans'
                                ] 
                ,'hclust'     : [  
                                  'BENCHMARK_2_hclust.yml'
                                , 'rows_averages_by_cluster_hclust'
                                , 'rows_by_columns_heatmap_hclust'
-                               , 'top_rows_by_cluster_hclust'
                                , 'rows_variance_hclust'
+                               , 'samples_label_by_cluster_hclust'
+                               , 'top_rows_by_cluster_hclust'
                                ] 
                ,'hclust_link': [  
                                  'BENCHMARK_3_hclust_link.yml'
                                , 'rows_averages_by_cluster_hclust_link'
                                , 'rows_by_columns_heatmap_hclust_link'
-                               , 'top_rows_by_cluster_hclust_link'
                                , 'rows_variance_hclust_link'
+                               , 'samples_label_by_cluster_hclust_link'
+                               , 'top_rows_by_cluster_hclust_link'
                                ]
                 }
 
