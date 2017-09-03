bb3a050c5aa69898951ed3723d672fed07c91bd5
Nahil Sobh <nahil.sobh@gmail.com>
2017-09-02 21:46:38 -0500

    refactoring

--- src/general_clustering_toolbox.py
+++ src/general_clustering_toolbox.py
@@ -12,57 +12,6 @@ from sklearn.metrics   import silhouette_score
 import knpackage.toolbox as kn
 import knpackage.distributed_computing_utils as dstutil
 
-def run_hclust(run_parameters):
-    """ wrapper: call sequence to perform hierchical clustering and save the results.
-
-    Args:
-        run_parameters: parameter set dictionary.
-    """
-
-    number_of_clusters         = run_parameters['number_of_clusters']
-    spreadsheet_name_full_path = run_parameters['spreadsheet_name_full_path']
-    spreadsheet_df             = kn.get_spreadsheet_df(spreadsheet_name_full_path)
-    spreadsheet_mat            = spreadsheet_df.as_matrix()
-    number_of_samples          = spreadsheet_mat.shape[1]
-    # ------------------------------------------------------------------------------
-    #h_mat                      = kn.perform_hclust(spreadsheet_mat, run_parameters)
-    # ------------------------------------------------------------------------------
-    ward                       = AgglomerativeClustering(n_clusters   = number_of_clusters, 
-                                                         linkage      = 'ward'            ).fit(spreadsheet_mat.T)
-    labels                     = ward.labels_
-    sample_names               = spreadsheet_df.columns
-
-    save_final_samples_clustering(sample_names, labels, run_parameters)
-    save_spreadsheet_and_variance_heatmap(spreadsheet_df, labels, run_parameters)
-    return labels
-
-def run_hclust_link(run_parameters):
-    """ wrapper: call sequence to perform hierchical clustering and save the results.
-
-    Args:
-        run_parameters: parameter set dictionary.
-    """
-
-    nearest_neighbors          = run_parameters['nearest_neighbors']
-    number_of_clusters         = run_parameters['number_of_clusters']
-    spreadsheet_name_full_path = run_parameters['spreadsheet_name_full_path']
-    spreadsheet_df             = kn.get_spreadsheet_df(spreadsheet_name_full_path)
-    spreadsheet_mat            = spreadsheet_df.as_matrix()
-    number_of_samples          = spreadsheet_mat.shape[1]
-    # ------------------------------------------------------------------------------
-    #h_mat                      = kn.perform_hclust(spreadsheet_mat, run_parameters)
-    # ------------------------------------------------------------------------------
-    connectivity = kneighbors_graph(spreadsheet_mat.T, n_neighbors=nearest_neighbors, include_self=False)
-    ward                       = AgglomerativeClustering(n_clusters   = number_of_clusters, 
-                                                         connectivity = connectivity      , 
-                                                         linkage      = 'ward'            ).fit(spreadsheet_mat.T)
-    labels                     = ward.labels_
-    sample_names               = spreadsheet_df.columns
-
-    save_final_samples_clustering(sample_names, labels, run_parameters)
-    save_spreadsheet_and_variance_heatmap(spreadsheet_df, labels, run_parameters)
-    return labels
-
 def run_kmeans(run_parameters):
     """ wrapper: call sequence to perform kmeans clustering and save the results.
 
@@ -70,58 +19,74 @@ def run_kmeans(run_parameters):
         run_parameters: parameter set dictionary.
     """
 
-    number_of_clusters         = run_parameters['number_of_clusters']
+    number_of_clusters         = run_parameters['number_of_clusters'        ]
     spreadsheet_name_full_path = run_parameters['spreadsheet_name_full_path']
+
     spreadsheet_df             = kn.get_spreadsheet_df(spreadsheet_name_full_path)
     spreadsheet_mat            = spreadsheet_df.as_matrix()
     number_of_samples          = spreadsheet_mat.shape[1]
+
     labels                     = kn.perform_kmeans(spreadsheet_mat.T, number_of_clusters)
     sample_names               = spreadsheet_df.columns
 
-    save_final_samples_clustering(sample_names, labels, run_parameters)
+    save_final_samples_clustering        (sample_names  , labels, run_parameters)
     save_spreadsheet_and_variance_heatmap(spreadsheet_df, labels, run_parameters)
+
     return labels
 
-def save_spreadsheet_and_variance_heatmap(spreadsheet_df, labels, run_parameters, network_mat=None):
-    """ save the full rows by columns spreadsheet as processed or smoothed if network is provided.
-        Also save variance in separate file.
+def run_hclust(run_parameters):
+    """ wrapper: call sequence to perform hierchical clustering and save the results.
+
     Args:
-        spreadsheet_df: the dataframe as processed
-        run_parameters: with keys for "results_directory", "method", (optional - "top_number_of_rows")
-        network_mat:    (if appropriate) normalized network adjacency matrix used in processing
-
-    Output:
-        rows_by_samples_heatmp_{method}_{timestamp}_viz.tsv
-        rows_averages_by_cluster_{method}_{timestamp}_viz.tsv
-        top_rows_by_cluster_{method}_{timestamp}_download.tsv
+        run_parameters: parameter set dictionary.
     """
-    clusters_df = spreadsheet_df
-    clusters_df.to_csv(get_output_file_name(run_parameters, 'rows_by_columns_heatmap', 'viz'), sep='\t')
 
-    cluster_ave_df = pd.DataFrame({i: spreadsheet_df.iloc[:, labels == i].mean(axis=1) for i in np.unique(labels)})
+    number_of_clusters         = run_parameters['number_of_clusters'        ]
+    spreadsheet_name_full_path = run_parameters['spreadsheet_name_full_path']
 
-    col_labels = []
-    for cluster_number in np.unique(labels):
-        col_labels.append('Cluster_%d'%(cluster_number))
+    spreadsheet_df             = kn.get_spreadsheet_df(spreadsheet_name_full_path)
+    spreadsheet_mat            = spreadsheet_df.as_matrix()
+    number_of_samples          = spreadsheet_mat.shape[1]
 
-    cluster_ave_df.columns = col_labels
-    cluster_ave_df.to_csv(get_output_file_name(run_parameters, 'rows_averages_by_cluster', 'viz'), sep='\t')
+    ward                       = AgglomerativeClustering( n_clusters   = number_of_clusters  
+                                                        , linkage      = 'ward'            ).fit(spreadsheet_mat.T)
+    labels                     = ward.labels_
+    sample_names               = spreadsheet_df.columns
 
-    clusters_variance_df = pd.DataFrame(clusters_df.var(axis=1), columns=['variance'])
-    clusters_variance_df.to_csv(get_output_file_name(run_parameters, 'rows_variance', 'viz'), sep='\t')
+    save_final_samples_clustering        (sample_names  , labels, run_parameters)
+    save_spreadsheet_and_variance_heatmap(spreadsheet_df, labels, run_parameters)
 
-    top_number_of_rows_df = pd.DataFrame(data=np.zeros((cluster_ave_df.shape)), columns=cluster_ave_df.columns,
-                                          index=cluster_ave_df.index.values)
+    return labels
 
-    top_number_of_rows = run_parameters['top_number_of_rows']
-    for sample in top_number_of_rows_df.columns.values:
-        top_index = np.argsort(cluster_ave_df[sample].values)[::-1]
-        top_number_of_rows_df[sample].iloc[top_index[0:top_number_of_rows]] = 1
-    top_number_of_rows_df.to_csv(get_output_file_name(run_parameters, 'top_rows_by_cluster', 'download'), sep='\t')
+def run_hclust_link(run_parameters):
+    """ wrapper: call sequence to perform hierchical clustering using linkage and save the results.
 
+    Args:
+        run_parameters: parameter set dictionary.
+    """
+
+    nearest_neighbors          = run_parameters['nearest_neighbors'         ]
+    number_of_clusters         = run_parameters['number_of_clusters'        ]
+    spreadsheet_name_full_path = run_parameters['spreadsheet_name_full_path']
+
+    spreadsheet_df             = kn.get_spreadsheet_df(spreadsheet_name_full_path)
+    spreadsheet_mat            = spreadsheet_df.as_matrix()
+    number_of_samples          = spreadsheet_mat.shape[1]
+
+    connectivity               = kneighbors_graph(spreadsheet_mat.T, n_neighbors=nearest_neighbors, include_self=False)
+    ward                       = AgglomerativeClustering( n_clusters   = number_of_clusters 
+                                                        , connectivity = connectivity       
+                                                        , linkage      = 'ward'            ).fit(spreadsheet_mat.T)
+    labels                     = ward.labels_
+    sample_names               = spreadsheet_df.columns
+
+    save_final_samples_clustering        (sample_names  , labels, run_parameters)
+    save_spreadsheet_and_variance_heatmap(spreadsheet_df, labels, run_parameters)
+
+    return labels
 
 def save_final_samples_clustering(sample_names, labels, run_parameters):
-    """ wtite .tsv file that assings a cluster number label to the sample_names.
+    """ write .tsv file that assings a cluster number label to the sample_names.
 
     Args:
         sample_names: (unique) data identifiers.
@@ -132,11 +97,50 @@ def save_final_samples_clustering(sample_names, labels, run_parameters):
         samples_labeled_by_cluster_{method}_{timestamp}_viz.tsv
         phenotypes_labeled_by_cluster_{method}_{timestamp}_viz.tsv
     """
-#    cluster_labels_df = kn.create_df_with_sample_labels(sample_names, labels)
-    cluster_labels_df = pd.DataFrame(data=labels, index=sample_names)
 
     cluster_mapping_full_path = get_output_file_name(run_parameters, 'samples_label_by_cluster', 'viz')
-    cluster_labels_df.to_csv(cluster_mapping_full_path, sep='\t', header=None)
+    cluster_labels_df         = kn.create_df_with_sample_labels(sample_names, labels)
+    cluster_labels_df.to_csv  (cluster_mapping_full_path, sep='\t', header=None)
+
+
+def save_spreadsheet_and_variance_heatmap(spreadsheet_df, labels, run_parameters):
+    """ save the full rows by columns spreadsheet.
+        Also save variance in separate file.
+    Args:
+        spreadsheet_df: the dataframe as processed
+        run_parameters: with keys for "results_directory", "method", (optional - "top_number_of_rows")
+
+    Output:
+        rows_by_samples_heatmp_{method}_{timestamp}_viz.tsv
+        rows_averages_by_cluster_{method}_{timestamp}_viz.tsv
+        top_rows_by_cluster_{method}_{timestamp}_download.tsv
+    """
+
+    clusters_df     = spreadsheet_df
+    cluster_ave_df  = pd.DataFrame({i: spreadsheet_df.iloc[:, labels == i].mean(axis=1) for i in np.unique(labels)})
+
+    col_labels  = []
+    for cluster_number in np.unique(labels):
+        col_labels.append('Cluster_%d'%(cluster_number))
+    cluster_ave_df.columns = col_labels
+
+    clusters_variance_df  = pd.DataFrame( clusters_df.var(axis=1)
+                                        , columns=['variance']                  )
+    top_number_of_rows_df = pd.DataFrame( data=np.zeros((cluster_ave_df.shape))
+                                        , columns=cluster_ave_df.columns
+                                        , index=cluster_ave_df.index.values     )
+
+    print(run_parameters)
+
+    top_number_of_rows = 10 #run_parameters['top_number_of_rows']
+    for sample in top_number_of_rows_df.columns.values:
+        top_index                                                           = np.argsort(cluster_ave_df[sample].values)[::-1]
+        top_number_of_rows_df[sample].iloc[top_index[0:top_number_of_rows]] = 1
+
+    clusters_df.to_csv          (get_output_file_name(run_parameters, 'rows_by_columns_heatmap' , 'viz'), sep='\t')
+    cluster_ave_df.to_csv       (get_output_file_name(run_parameters, 'rows_averages_by_cluster', 'viz'), sep='\t')
+    clusters_variance_df.to_csv (get_output_file_name(run_parameters, 'rows_variance',            'viz'), sep='\t')
+    top_number_of_rows_df.to_csv(get_output_file_name(run_parameters, 'top_rows_by_cluster', 'download'), sep='\t')
 
 
 def get_output_file_name(run_parameters, prefix_string, suffix_string='', type_suffix='tsv'):
